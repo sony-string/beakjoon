2f9beb86832d1ca81470419cbe7c439ddd98e48a
SON-HYUN <sctoa1@gmail.com>
2023-12-17 22:16:41 +0900

    [G4] 1715 카드 정렬하기 (cpp)

--- Beakjoon/1715-cardSorting.cpp
+++ Beakjoon/1715-cardSorting.cpp
@@ -1,12 +1,17 @@
-#include <xkeycheck.h>
-#define private public
-#define protected public
 #include <cstdio>
 #include <queue>
 using namespace std;
 
+template <typename T, typename _Container = vector<T>, typename _Comp = less<T>>
+class P : public priority_queue<T, _Container, _Comp> {
+public:
+	int& operator[](unsigned int i) {
+		return c[i];
+	}
+};
+
 int main() {
-	priority_queue<int, vector<int>, greater<int>> pq;
+	P<int, vector<int>, greater<int>> pq;
 	int N;
 	int ans = 0;
 	scanf("%d", &N);
@@ -17,11 +22,11 @@ int main() {
 	}
 	while (pq.size() > 2) {		
 		int n;
-		ans += n = pq.c[0] + min(pq.c[1], pq.c[2]);
+		ans += n = pq[0] + min(pq[1], pq[2]);
 		pq.pop(); pq.pop();
 		pq.push(n);
 	}
-	if (N > 1) ans += pq.c[0] + pq.c[1];
+	if (N > 1) ans += pq[0] + pq[1];
 	printf("%d", ans);
 	return 0;
 }
